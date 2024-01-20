c329396f2ce9681959d815b4e59eb372e3bb474a
Евгений Подольский <evgeniy@podolskiyrsb.ru>
2024-01-20 20:33:15 +0300

    feat: ТЗ9

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,91 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.validation.ValidationException;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @RestController
 public class FilmController {
+
+    private Map<Integer, Film> films = new HashMap<>();
+    private int generatorId = 1;
+
+    @PostMapping("/films")
+    public Film addFilm(@RequestBody Film film) {
+        if (film != null) {
+            if (film.getName().isEmpty()) {
+                log.error("Фильм не имеет названия");
+                throw new ValidationException("Фильм должен иметь название");
+            }
+
+            if (film.getDescription().length() > 200) {
+                log.error("Описание фильма превышает лимит в 200 символов");
+                throw new ValidationException("Описание фильма не должно превышать более 200 символов");
+            }
+
+            if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+                log.error("Релиз фильма состоялся раньше 28 декабря 1895 года");
+                throw new ValidationException("Релиз фильма не мог состояться раньше 28 декабря 1895 года");
+            }
+
+            if (film.getDuration() < 0) {
+                log.error("Продолжительность фильма отрицательное число");
+                throw new ValidationException("Продолжительность фильма не может быть отрицательным числом");
+            }
+            film.setId(generatorId++);
+            films.put(film.getId(), film);
+        } else {
+            log.error("Передан пустой объект");
+            throw new ValidationException("Объект не может быть пустым");
+        }
+        return film;
+    }
+
+    @PutMapping("/films")
+    public Film updateFilm(@RequestBody Film film) {
+        if (film != null && films.containsKey(film.getId())) {
+            Film updateFilm = films.get(film.getId());
+            if (film.getName().isEmpty()) {
+                log.error("Фильм не имеет названия");
+                throw new ValidationException("Фильм должен иметь название");
+            }
+
+            if (film.getDescription().length() > 200) {
+                log.error("Описание фильма превышет лимит в 200 символов");
+                throw new ValidationException("Описание фильма не должно превышать более 200 символов");
+            }
+
+            if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+                log.error("Релиз фильма состоялся раньше 28 декабря 1895 года");
+                throw new ValidationException("Релиз фильма не мог состояться раньше 28 декабря 1895 года");
+            }
+
+            if (film.getDuration() < 0) {
+                log.error("Продолжительность фильма отрицательное число");
+                throw new ValidationException("Продолжительность фильма не может быть отрицательным числом");
+            }
+            updateFilm.setName(film.getName());
+            updateFilm.setDescription(film.getDescription());
+            updateFilm.setReleaseDate(film.getReleaseDate());
+            updateFilm.setDuration(film.getDuration());
+            films.put(film.getId(), updateFilm);
+        } else {
+            log.error("Передан пустой объект");
+            throw new ValidationException("Объект не может быть пустым");
+        }
+        return film;
+    }
+
+    @GetMapping("/films")
+    public List<Film> getFilms() {
+        return new ArrayList<>(films.values());
+    }
 }
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,146 @@
 package ru.yandex.practicum.filmorate;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.validation.ValidationException;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FilmorateApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private FilmController filmController;
+    private UserController userController;
 
+    @BeforeEach
+    public void setUp() {
+        filmController = new FilmController();
+        userController = new UserController();
+    }
+
+    @Test
+    public void addNewFilmResultTrue() {
+        Film film = new Film(1, "Интерстеллар", "Про космос",
+                LocalDate.of(2014, 11, 6), 169);
+        assertEquals(0, filmController.getFilms().size());
+
+        filmController.addFilm(film);
+
+        assertEquals(1, filmController.getFilms().size());
+    }
+
+    @Test
+    public void addNewFilmNull() {
+        Assertions.assertThrows(ValidationException.class, () -> filmController.addFilm(null));
+    }
+
+    @Test
+    public void addNewFilmWithoutName() {
+        Film film = new Film(1, "", "Про космос",
+                LocalDate.of(2014, 11, 6), 169);
+        Assertions.assertThrows(ValidationException.class, () -> filmController.addFilm(film));
+    }
+
+    @Test
+    public void addNewFilmWithDescriptionMore200Symbols() {
+        Film film = new Film(1, "Интерстеллар", "Когда засуха, пыльные бури и вымирание растений" +
+                " приводят человечество к продовольственному кризису, коллектив исследователей и учёных отправляется" +
+                " сквозь червоточину (которая предположительно соединяет области пространства-времени через большое" +
+                " расстояние) в путешествие, чтобы превзойти прежние ограничения для космических путешествий человека" +
+                " и найти планету с подходящими для человечества условиями.\n" +
+                "\n",
+                LocalDate.of(2014, 11, 6), 169);
+
+        Assertions.assertThrows(ValidationException.class, () -> filmController.addFilm(film));
+    }
+
+    @Test
+    public void addNewFilmWithDateReleaseBeforeFirstDate() {
+        Film film = new Film(1, "Интерстеллар", "Про космос",
+                LocalDate.of(1800, 11, 6), 169);
+
+        Assertions.assertThrows(ValidationException.class, () -> filmController.addFilm(film));
+    }
+
+    @Test
+    public void addNewFilmWithNegativeDuration() {
+        Film film = new Film(1, "Интерстеллар", "Про космос",
+                LocalDate.of(2014, 11, 6), -169);
+
+        Assertions.assertThrows(ValidationException.class, () -> filmController.addFilm(film));
+    }
+
+    @Test
+    public void addNewUser() {
+        User user = new User(1, "evgeniy@yandex.ru", "evgen", "Evgeniy",
+                LocalDate.of(1999, 12, 18));
+
+        assertEquals(0, userController.getUsers().size());
+
+        userController.addUser(user);
+        assertEquals(1, userController.getUsers().size());
+    }
+
+    @Test
+    public void addNewUserNull() {
+        Assertions.assertThrows(ValidationException.class, () -> userController.addUser(null));
+    }
+
+    @Test
+    public void addNewUserWithoutEmail() {
+        User user = new User(1, "", "evgen", "Evgeniy",
+                LocalDate.of(1999, 12, 18));
+
+        Assertions.assertThrows(ValidationException.class, () -> userController.addUser(user));
+    }
+
+    @Test
+    public void addNewUserWithoutInEmailSymbol() {
+        User user = new User(1, "evgeniy.yandex.ru", "evgen", "Evgeniy",
+                LocalDate.of(1999, 12, 18));
+
+        Assertions.assertThrows(ValidationException.class, () -> userController.addUser(user));
+    }
+
+    @Test
+    public void addNewUserWithoutLogin() {
+        User user = new User(1, "evgeniy@yandex.ru", "", "Evgeniy",
+                LocalDate.of(1999, 12, 18));
+
+        Assertions.assertThrows(ValidationException.class, () -> userController.addUser(user));
+    }
+
+    @Test
+    public void addNewUserWithBlankInLogin() {
+        User user = new User(1, "evgeniy@yandex.ru", "ev gen", "Evgeniy",
+                LocalDate.of(1999, 12, 18));
+
+        Assertions.assertThrows(ValidationException.class, () -> userController.addUser(user));
+    }
+
+    @Test
+    public void addNewUserWithoutName() {
+        User user = new User(1, "evgeniy@yandex.ru", "evgen", "",
+                LocalDate.of(1999, 12, 18));
+
+        userController.addUser(user);
+        assertEquals(1, userController.getUsers().size());
+        assertEquals("evgen", user.getName());
+    }
+
+    @Test
+    public void addNewUserWithBirthdayInFuture() {
+        User user = new User(1, "evgeniy@yandex.ru", "evgen", "Evgeniy",
+                LocalDate.of(9999, 12, 18));
+
+        Assertions.assertThrows(ValidationException.class, () -> userController.addUser(user));
+    }
 }
